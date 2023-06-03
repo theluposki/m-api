a2365548a10765c2ea87c40ef24fce372d69caec
theluposki <theluposki@gmail.com>
2023-06-03 07:36:23 -0300

    redis

--- app/src/socket.io.js
+++ app/src/socket.io.js
@@ -11,13 +11,10 @@ export const IO = (server) => {
   });
 
   io.on("connection", async (socket) => {
-    // Evento de conexão do Socket.IO
-
     socket.on("login", async (nickname) => {
       const socketId = socket.id;
 
       try {
-        // Verificar se o nickname já está em uso
         const existingUser = await redisClient.get(`nickname:${nickname}`);
         if (existingUser) {
           console.log(`Usuário ${nickname} já está logado.`);
@@ -26,14 +23,11 @@ export const IO = (server) => {
           return;
         }
 
-        // Armazenar o nickname do usuário vinculado ao socketId no Redis
         await redisClient.set(`nickname:${nickname}`, socketId);
         console.log(`Usuário ${nickname} conectado.`);
 
-        // Incrementar a contagem de usuários logados
         await redisClient.incr("onlineUserCount");
 
-        // Emitir evento com a contagem atualizada para todos os clientes
         const onlineUserCount = await redisClient.get("onlineUserCount");
         io.emit("userCountUpdate", onlineUserCount);
 
@@ -41,15 +35,10 @@ export const IO = (server) => {
 
         socket.on("disconnect", async () => {
           try {
-            // Evento de desconexão do cliente
-            // Remover o nickname do Redis
             await redisClient.del(`nickname:${nickname}`);
-
-            // Decrementar a contagem de usuários logados
             await redisClient.decr("onlineUserCount");
             console.log(`Usuário ${nickname} desconectado.`);
 
-            // Emitir evento com a contagem atualizada para todos os clientes
             const onlineUserCount = await redisClient.get("onlineUserCount");
             io.emit("userCountUpdate", onlineUserCount);
           } catch (error) {
